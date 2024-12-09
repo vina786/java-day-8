791c9d3601a6b7ca26842dc50523fd5b80e5a8eb
vina786 <vinay76b@gmail.com>
2024-12-09 13:33:00 +0530

    Add files via upload

---
+++ countarray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class countarray {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        arr[i]=sc.nextInt();
+        Arrays.sort(arr);
+        int max=arr[n-1];
+        int c[]=new int[max+1];
+        for(int i=0;i<n;i++)
+        c[arr[i]]++;
+        System.out.println(Arrays.toString(c));
+    }
+}

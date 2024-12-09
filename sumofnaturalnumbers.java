640101e79cb54b1479612590d51b220eb20fe86b
vina786 <vinay76b@gmail.com>
2024-12-09 11:49:36 +0530

    Add files via upload

---
+++ sumofnaturalnumbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class sumofnaturalnumbers {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int ns[]=new int[n];
+        int s=0;
+        int k= 0;
+        for(int i=1;i<=n;i++)
+        {
+            s+=i;
+            ns[k]=s;
+            k++;
+        }
+        System.out.println(Arrays.toString(ns));
+    }
+}

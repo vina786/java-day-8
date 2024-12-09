210dc5f97516b042e6f86ab0a026971c52d51564
vina786 <vinay76b@gmail.com>
2024-12-09 15:10:32 +0530

    Add files via upload

---
+++ twodmatrix.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class twodmatrix {
+    public static void main(String args[])
+    {
+        //initallization of 2d array
+        int a[] []={{1,2},{3,4}};
+        //accessing 1d array in 2d array
+        System.out.println(Arrays.toString(a[0]));
+        System.out.println(Arrays.toString(a[1]));
+    }
+    
+    
+}
